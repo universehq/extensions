6aa76086dcd43f1d7380509c53cb98c2ebe5d1aa
Ryan Martin <54093748+eqpoqpe@users.noreply.github.com>
2025-10-22 15:22:43 +0800

    Create RangeExtension.cs

---
+++ src/Universe.Extensions.Syntactic/RangeExtension.cs
@@ -0,0 +1,10 @@
+namespace Universe.Extensions.Syntactic;
+
+public static class IntExtensions
+{
+    public static IEnumerable<int> To(this int start, int end)
+    {
+        for (int i = start; i <= end; i++)
+            yield return i;
+    }
+}
