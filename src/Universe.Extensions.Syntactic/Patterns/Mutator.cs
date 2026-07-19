1a0e84959460b357ee333165b11ceedec21a69f0
Ryan Martin <eqpoqpe@gmail.com>
2026-07-19 12:48:08 +0800

    Refactor project files and remove deprecated extensions for cleaner structure

--- src/Universe.Extensions.Syntactic/Patterns/Mutator.cs
+++
@@ -1,9 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-namespace Universe.Extensions.Syntactic.Patterns;
-
-[Experimental("Mutator")]
-public delegate void Mutator<T>(T value);
-
-[Experimental("RefMutator")]
-public delegate void RefMutator<T>(ref T value);
